ba1cd4c27984413b5131b9261e8e52792d82dd9f
Aaqil Shihab <aaqilsh@yahoo.com>
2025-07-11 11:24:36 +0100

    Added resume record mdoel

--- models.py
+++ models.py
@@ -10,3 +10,9 @@ class Job(SQLModel, table=True):
     jd_text: str                      
     created_at: datetime = Field(default_factory=datetime.timezone.utc)
 
+
+class ResumeRecord(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    filename: str
+    text: str                         # extracted plain text
+    uploaded_at: datetime = Field(default_factory=datetime.utcnow)
