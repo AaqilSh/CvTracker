4994afe2ee6b62096ee94036e01d2806b79cfd4e
Aaqil Shihab <aaqilsh@yahoo.com>
2025-07-12 11:33:06 +0100

    Add JobRead model to extend JobCreate with id and created_at fields

--- schemas.py
+++ schemas.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class JobCreate(BaseModel):
     title: str
     company: str
     status: str = "Applied"
     jd_text: str
+
+class JobRead(JobCreate):
+    id: int
+    created_at: datetime
