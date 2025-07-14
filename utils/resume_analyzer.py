9000c274b49ae2c27e06c397b4c0b6d3b1c72372
Aaqil Shihab <aaqilsh@yahoo.com>
2025-07-14 18:31:43 +0100

    Add extract_keywords function to resume_analyzer for keyword extraction

--- utils/resume_analyzer.py
+++ utils/resume_analyzer.py
@@ -3,3 +3,6 @@ from sentence_transformers import SentenceTransformer
 
 _kw_model = KeyBERT()
 _emb_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def extract_keywords(text: str, top_n: int = 20) -> List[str]:
+    return [kw for kw, _ in _kw_model.extract_keywords(text, top_n=top_n)]
