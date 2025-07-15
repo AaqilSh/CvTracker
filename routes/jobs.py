5b40cfd8dc7d080f8934c9ac8e45ce6ad39af733
Aaqil Shihab <aaqilsh@yahoo.com>
2025-07-15 17:39:45 +0100

    Refactor jobs route to include necessary imports and define add_job function

--- routes/jobs.py
+++ routes/jobs.py
@@ -1,3 +1,12 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from crud import crud
+from schemas import schemas
+from database import get_session
+from sqlmodel import Session
+from models import models
 router = APIRouter(prefix="/jobs", tags=["Jobs"])
+
+@router.post("/", response_model=schemas.JobRead)
+def add_job(job: schemas.JobCreate, session: Session = Depends(get_session)):
+    db_job = crud.create_job(session, models.Job(**job.dict()))
+    return db_job
